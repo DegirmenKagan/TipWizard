dc691e86d2f5872abe5d43dcb3587f91e9906e12
Kağan Değirmen <kagandegirmen@gmail.com>
2024-01-07 18:17:58 +0300

    Initial Commit

---
+++ TipWizard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipWizard
+//
+//  Created by Kağan Değirmen on 7.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
