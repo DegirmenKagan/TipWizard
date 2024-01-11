1e5fd4b1132654e73869360b15f3083c227bcddb
Kağan Değirmen <kagandegirmen@gmail.com>
2024-01-11 21:48:34 +0300

    ResultsViewController PART 1

---
+++ TipWizard/Controllers/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  TipWizard
+//
+//  Created by Kağan Değirmen on 11.01.2024.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
