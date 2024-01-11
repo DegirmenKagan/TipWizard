d21e2808a543be2d7a4541b5168f46587a8076e5
Kağan Değirmen <kagandegirmen@gmail.com>
2024-01-11 21:16:41 +0300

    CalculatorViewController > buttons labels linked

---
+++ TipWizard/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorViewController.swift
+//  TipWizard
+//
+//  Created by Kağan Değirmen on 7.01.2024.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+}
+
