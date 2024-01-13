5a6d807ed5ead1d5b6af1e2b9a02bc37873e2844
Kağan Değirmen <kagandegirmen@gmail.com>
2024-01-14 00:15:58 +0300

    Functionality implemented on CalculatorView

--- TipWizard/Controllers/CalculatorViewController.swift
+++ TipWizard/Controllers/CalculatorViewController.swift
@@ -14,17 +14,32 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    var tip = 0.10
+    var splitNumber = 2
 
     @IBAction func tipChanged(_ sender: UIButton) {
-        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        let buttonTitle = sender.currentTitle!
+        let buttonTip = String(buttonTitle).dropLast()
+        tip = Double(buttonTip)! / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumber = Int(sender.value)
+        splitNumberLabel.text = String(splitNumber)
         
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         
+        var billTotal = Float(billTextField.text!)!
+        var billPlusTip = billTotal + (billTotal * Float(tip))
+        var totalPerPerson = billPlusTip / Float(splitNumber)
+        
+        print(totalPerPerson)
     }
 }
 
--- TipWizard/Controllers/ResultsViewController.swift
+++ TipWizard/Controllers/ResultsViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
